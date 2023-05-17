a25446a8d3a06b39ff9db2cfd819abee439fb534
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-16 21:18:51 -0400

    Initial Commit

---
+++ ModManager/ModManager/Main.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace ModManager
+{
+    [BepInPlugin(GUID, NAME, VERSION), BepInDependency("Crafterbot.MonkeStatistics")]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "crafterbot.gtag.modmanager",
+            NAME = "Mod Toggle",
+            VERSION = "1.0.0";
+        internal static ManualLogSource manualLogSource;
+
+        private void Awake() =>
+            MonkeStatistics.API.Registry.AddAssembly();
+    }
+
+    internal static class Extensions
+    {
+        internal static void Log(this object obj, LogLevel logLevel = LogLevel.Info)
+        {
+#if DEBUG
+            Main.manualLogSource.Log(logLevel, obj);
+#endif
+        }
+    }
+}
---
+++ ModManager/ModManager/Pages/PrimaryPage.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx.Bootstrap;
+using HarmonyLib;
+using MonkeStatistics.API;
+
+namespace ModManager.Pages
+{
+    [DisplayInMainMenu(Main.NAME)]
+    internal class PrimaryPage : Page
+    {
+        public override void OnPageOpen()
+        {
+            base.OnPageOpen();
+
+            foreach (var plugin in Chainloader.PluginInfos)
+                if (plugin.Key != Main.GUID)
+                    if (AccessTools.Method(plugin.Value.Instance.GetType(), "OnEnable") != null && AccessTools.Method(plugin.Value.Instance.GetType(), "OnDisable") != null) // got this idea from computer interface by ToniMacaroni
+                        AddLine(plugin.Value.Metadata.Name, new ButtonInfo((object Sender, object[] Args) => { plugin.Value.Instance.enabled = (bool)Args[1]; }, 0, ButtonInfo.ButtonType.Toggle, plugin.Value.Instance.enabled));
+                    else
+                        AddLine(plugin.Value.Metadata.Name + " (UNSUPPORTED)");
+            SetTitle(Main.NAME);
+            SetAuthor("By Crafterbot");
+            SetLines();
+        }
+    }
+}
